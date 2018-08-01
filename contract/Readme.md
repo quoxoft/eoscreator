a43f86a522dfd5979791063b11760cc6d2d62b7f
quoc le <songmai108@gmail.com>
2018-08-01 10:19:41 +0700

    User can do transation

--- contract/Readme.md
+++ contract/Readme.md
@@ -1,4 +1,4 @@
-new account smart contract on EOS
+New account smart contract on EOS
 -------
 #### Prerequisites
 * Wallet must be unlock and have at least the following private keys
@@ -15,7 +15,41 @@ new account smart contract on EOS
 cleos set contract newaccount ./new_account -p newaccount
 ````
 
-##### Set eosio.token contract to eosio.token account
+##### Make a transaction with memo follow rule: AccountName-OwnerPubKey-AcvitePubKey
 ````bash
-cleos set contract eosio.token build/contracts/eosio.token -p eosio.token
+$ cleos transfer helloeos newaccount "2 EOS" "helloeos1234-EOS68sM3gfTtxjBawvCBDnCxWgG1EN1jEGcqCD2H7mRJDJkmamu7R-EOS4zf7TVoYbsxFXpSc1J9HPkMn1r86jjpEVg6BnKL9SUMeJ7ThSt" -p helloeos
 ````
+
+##### Get result
+$ cleos get account helloeos1234
+
+````bash
+permissions: 
+     owner     1:    1 EOS4zf7TVoYbsxFXpSc1J9HPkMn1r86jjpEVg6BnKL9SUMeJ7ThSt
+        active     1:    1 EOS68sM3gfTtxjBawvCBDnCxWgG1EN1jEGcqCD2H7mRJDJkmamu7R
+memory: 
+     quota:     4.227 KiB    used:     3.365 KiB  
+
+net bandwidth: 
+     staked:          0.1000 EOS           (total stake delegated from account to self)
+     delegated:       0.0000 EOS           (total staked delegated to account from others)
+     used:                 0 bytes
+     available:        19.23 KiB  
+     limit:            19.23 KiB  
+
+cpu bandwidth:
+     staked:          0.1000 EOS           (total stake delegated from account to self)
+     delegated:       0.0000 EOS           (total staked delegated to account from others)
+     used:                 0 us   
+     available:        3.752 ms   
+     limit:            3.752 ms   
+
+EOS balances: 
+     liquid:            0.9967 EOS
+     staked:            0.2000 EOS
+     unstaking:         0.0000 EOS
+     total:             1.1967 EOS
+
+producers:     <not voted>
+````
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,42 +1,101 @@
-import eos from 'eosjs'
+import Eos from 'eosjs'
 import ecc from 'eosjs-ecc'
 
-const CONTRACT_NAME = 'helloeos'
-const LOCAL_TESTNET= 'http://127.0.0.1:8888'
+const CONTRACT_NAME = 'newaccount'
+const SYSTEM_CONTRACT_NAME = 'eosio'
+const LOCAL_TESTNET = 'http://127.0.0.1:8888'
 const JUNGLE_TESTNET = 'http://dev.cryptolions.io:38888'
 const MAINNET = 'http://mainnet.libertyblock.io:8888'
+var ramKBPrice = 0;
 
-//Eos = require('eosjs')
-window.genarateOwnerKey = function() {
+getRamKBPrice()
+
+function getEos() {
+    var privateKey = document.getElementById('transfer_key').value;
+    var config = {
+        keyProvider: [privateKey],
+        httpEndpoint: JUNGLE_TESTNET,
+        broadcast: true,
+        sign: true,
+        chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+        expireInSeconds: 60
+    }
+    return Eos(config);
+}
+
+window.genarateOwnerKey = function () {
     ecc.randomKey().then(privateKey => {
         console.log('Private Key:\t', privateKey) // wif
         console.log('Public Key:\t', ecc.privateToPublic(privateKey)) // EOSkey...
         document.getElementById("owner_prvkey").value = privateKey;
-        document.getElementById("owner_pubkey").value = ecc.privateToPublic(privateKey);       
-        })
+        document.getElementById("owner_pubkey").value = ecc.privateToPublic(privateKey);
+    })
 }
 
-window.genarateActiveKey = function() {
+window.genarateActiveKey = function () {
     ecc.randomKey().then(privateKey => {
         console.log('Private Key:\t', privateKey) // wif
         console.log('Public Key:\t', ecc.privateToPublic(privateKey)) // EOSkey...
         document.getElementById("active_prvkey").value = privateKey;
-        document.getElementById("active_pubkey").value = ecc.privateToPublic(privateKey);       
-        })
+        document.getElementById("active_pubkey").value = ecc.privateToPublic(privateKey);
+    })
 }
 
-window.createAccount = function() {
-    var owner_pubkey = document.getElementById('owner_pubkey').value;    
+window.createAccount = function () {
+    var owner_pubkey = document.getElementById('owner_pubkey').value;
     var active_pubkey = document.getElementById('active_pubkey').value;
     var account_name = document.getElementById('eos_account').value;
 
-    if((ecc.isValidPublic(owner_pubkey) === true)&&(ecc.isValidPublic(active_pubkey) === true)){
-        $("#msg-success").html("Please transfer to <b> newaccount</b> at least <b> 2 EOS</b> with following <b>memo</b>, The smart contract will automatically create your account");
-        document.getElementById("msg-memo").value = account_name + "-" + owner_pubkey+ "-" + active_pubkey;
-
-    }else{
-        $("#msg-warning").html("Please check, There are something wrong");
+    if (ecc.isValidPublic(owner_pubkey) && ecc.isValidPublic(active_pubkey) && (account_name.length == 12)) {
+        console.log(getCreatingAccountPrice())
+        $("#msg-success").html("Please transfer to <b> newaccount</b> at least <b>" + getCreatingAccountPrice() + " EOS</b> with following <b>memo</b>, The smart contract will automatically create your account");
+        document.getElementById("msg-memo").value = account_name + "-" + owner_pubkey + "-" + active_pubkey;
+    } else if (account_name.length != 12) {
+        $("#msg-warning").html("Please check your account name");
+    } else {
+        $("#msg-warning").html("Please check the key pair");
     }
+}
+
+window.transfer = function () {
+    var transfer_from = document.getElementById('transfer_from').value;
+    var transfer_key = document.getElementById('transfer_key').value;
+    //var transfer_to = document.getElementById('transfer_to').value;
+    var transfer_amount = document.getElementById('transfer_amount').value;
+    var transfer_amount_eos = transfer_amount + " EOS";
+    console.log(transfer_amount_eos)
+    var transfer_memo = document.getElementById('transfer_memo').value;
+
+    var eos = getEos();
+
+    eos.transfer(transfer_from, CONTRACT_NAME, transfer_amount_eos, transfer_memo, (error, result) => {console.log(error, result)})
+}
+
+function getCreatingAccountPrice() {
+    var netPrice = 0.1; // 0.1 EOS
+    var cpuPrice = 0.1; // 0.1 EOS
+    // to make sure we add more than 10%, the remain value will be added to your account
+    return (ramKBPrice * 4 + netPrice + cpuPrice) * 1.1;
+}
+
+function getRamKBPrice() {
+    var eos = getEos();
+    var params = {
+        json: true,
+        scope: SYSTEM_CONTRACT_NAME,
+        code: SYSTEM_CONTRACT_NAME,
+        table: "rammarket",
+    }
+
+    eos.getTableRows(params).then(resp => {
+        var ramBaseBalance = resp.rows[0].base.balance; // Amount of RAM bytes in use
+        ramBaseBalance = ramBaseBalance.substr(0, ramBaseBalance.indexOf(' '));
+        var ramQuoteBalance = resp.rows[0].quote.balance; // Amount of EOS in the RAM collector
+        ramQuoteBalance = ramQuoteBalance.substr(0, ramQuoteBalance.indexOf(' '));
+        ramKBPrice = (ramQuoteBalance / ramBaseBalance).toFixed(8) * 1024; // Price in kb
+    });
+
+    return ramKBPrice;
 }
 
 
@@ -44,5 +103,3 @@ window.createAccount = function() {
 
 
 
-
-  
